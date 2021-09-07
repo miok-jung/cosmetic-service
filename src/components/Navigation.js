329cf3e39172f8a696b2eddd3d5e5df65e1e7363
miok-jung <miok.dev@gmail.com>
2021-09-08 00:51:23 +0900

    feat: first settings

---
+++ src/components/Navigation.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import 'styles/navigation.scss';
+
+const Navigation = () => {
+  return (
+    <nav>
+      <h1>LOGO</h1>
+      <ul>
+        <li>menu1</li>
+        <li>menu1</li>
+        <li>menu1</li>
+        <li>menu1</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* HTML5의 History API를 사용하여 페이지를 새로고침하지 않도록 주소를 변경할 수 있도록 해준다.
+    즉, 페이지 변경으로 인한 깜빡거림이 없다.
+    또한, 현재 주소에 관련된 정보를 props로 조회 및 사용이 가능하도록 만들어준다. */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();

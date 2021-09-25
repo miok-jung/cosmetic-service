ff31f19b0c2b9a52c4ff2e06bf97988c8a80b548
miok-jung <miok.dev@gmail.com>
2021-09-25 20:43:51 +0900

    fix: Change to Auto Carousel, Modify to detail SCSS

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # Cosmetic Project
 
-## lanaguage
+이 프로젝트는 개인 리액트 공부를 위한 스터디 프로젝트이며, 라이브러리를 최소화와 직접 구현의 목적을 두었습니다.
 
-- React
+## Study Date
+
+- 2021 09 08 ~ ing
+
+## Recode
+
+- first Settings
+  - prettierrc, config, layout ( 2021. 09. 18 )
+- Carousel
+  - Add to Carousel ( 2021. 09. 22 )
+  - Fix to Carousel Auto ( 2021. 09. 25 )
--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ ReactDOM.render(
     {/* HTML5의 History API를 사용하여 페이지를 새로고침하지 않도록 주소를 변경할 수 있도록 해준다.
     즉, 페이지 변경으로 인한 깜빡거림이 없다.
     또한, 현재 주소에 관련된 정보를 props로 조회 및 사용이 가능하도록 만들어준다. */}
+    {/* <BrowserRouter basename="/cosmetic-service"> */}
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       {/* gh-pages 추가시 Local의 baseUrl이 변경이 됨
       http://localhost:3000/ 에서 http://localhost:3000/cosmetic-service 로 변경되어

91e1ac46e1dcaf246a019830fa82f51098d3db13
miok-jung <miok.dev@gmail.com>
2021-10-12 22:34:03 +0900

    feat: Add to footer

--- src/container/main/index.js
+++ src/container/main/index.js
@@ -3,15 +3,17 @@ import Navigation from 'components/Navigation';
 import AutoSlide from 'components/AutoSlide';
 import Container1 from 'components/Container1';
 import Container2 from 'components/Container2';
+import Footer from 'components/Footer';
 
 const Home = () => {
   return (
-    <div>
+    <>
       <Navigation />
       <AutoSlide />
       <Container1 />
       <Container2 />
-    </div>
+      <Footer />
+    </>
   );
 };
 

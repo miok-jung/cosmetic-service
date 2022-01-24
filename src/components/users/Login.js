1be19d3e257503a14eb10bbcf6036743e6c0e5d8
miok-jung <miok.dev@gmail.com>
2022-01-24 22:04:55 +0900

    modify: node-sass 버전 변경

--- src/components/users/Login.js
+++ src/components/users/Login.js
@@ -1,16 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+// import users from 'dummy/user.json';
 import 'styles/scss/users/login.scss';
 
-const Login = () => {
+const Login = props => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onEmailHandler = e => {
+    setEmail(e.currentTarget.value);
+  };
+  const onPasswordHandler = e => {
+    setPassword(e.currentTarget.value);
+  };
+  const onLogin = e => {
+    console.log('test');
+    e.preventDefault();
+    // if (email === '' || password === '') {
+    //   alert('이메일 혹은 비밀번호를 입력해주세요.');
+    //   return false;
+    // }
+    // props.history('/');
+  };
   return (
-    <article className="login-wrap">
+    <article clstateme="login-wrap">
       <div className="login-inner-wrap">
         <h2>로그인</h2>
         <form className="login-form">
-          <input type="email" placeholder="example@example.com" />
-          <input type="password" placeholder="Password" />
-          <button type="submit" className="login-btn">
+          <input
+            type="email"
+            placeholder="example@example.com"
+            value={email}
+            onChange={onEmailHandler}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={onPasswordHandler}
+          />
+          <button type="submit" className="login-btn" onSubmit={onLogin}>
             Login
           </button>
         </form>

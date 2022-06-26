33f345bd977a299f64e4ca227ee9ca6661504608
miok-jung <miok.dev@gmail.com>
2022-06-26 20:48:43 +0900

    modify: Change to Setting - 세팅 변경

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   endOfLine: 'lf',
   tabWidth: 2,
-  semi: true,
+  semi: false,
   singleQuote: true,
   trailingComma: 'all',
   printWidth: 120,
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,12 +1,12 @@
-const { createProxyMiddleware } = require("http-proxy-middleware"); // createProxyMiddleware를 http-proxy-middleware 라이브러리에서 불러오기
+const { createProxyMiddleware } = require('http-proxy-middleware') // createProxyMiddleware를 http-proxy-middleware 라이브러리에서 불러오기
 // TODO 좀 더 간단한(혹은 다양한) proxy해결법 알아보기
 
 module.exports = function (app) {
   app.use(
-    "/api",
+    '/api',
     createProxyMiddleware({
-      target: "http://localhost:5000",
+      target: 'http://localhost:5000',
       changeOrigin: true,
-    })
-  );
-};
+    }),
+  )
+}

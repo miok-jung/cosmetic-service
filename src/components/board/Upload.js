c0dd9c1c20ac71ac2c691f9940e32ece4aef3731
miok-jung <miok.dev@gmail.com>
2022-06-27 17:34:15 +0900

    modify: Upload Image preview - 이미지 업로드 및 미리보기 구현 CSS 수정 - 페이지별 사이즈 변수 추가작업

--- src/components/board/Upload.js
+++ src/components/board/Upload.js
@@ -1,77 +1,102 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
-import Header from 'components/Header';
-// import ImgUpload from './ImgUpload';
-import 'css/upload.scss';
-import { useSelector } from 'react-redux';
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import axios from 'axios'
+import Header from 'components/Header'
+import * as config from '../../config'
+import 'css/upload.scss'
 
 const Upload = () => {
-  const [Title, setTitle] = useState('');
-  const [Content, setContent] = useState('');
-  const [Img, setImg] = useState('');
+  const [Title, setTitle] = useState('')
+  const [Content, setContent] = useState('')
 
-  let navigate = useNavigate();
-  const user = useSelector((state) => state.user);
+  let navigate = useNavigate()
+  const user = useSelector((state) => state.user)
+
+  const [File, setFile] = useState()
+  const [Images, setImages] = useState()
+
+  const FileUpload = async (e) => {
+    let formData = new FormData()
+    let file = e.target.files[0]
+    setFile(file)
+    formData.append('image', e.target.files[0])
+
+    const config = { headers: { 'Content-Type': 'multipart/form-data' } }
+    await axios
+      .post('/api/board/image/upload', formData, config)
+      .then((res) => {
+        setImages(res.data.imageURL)
+        console.log('r', res.data.imageURL)
+      })
+      .catch((err) => {
+        console.log('ERR', err)
+      })
+  }
+
   const onSubmit = (e) => {
-    e.preventDefault();
+    e.preventDefault()
     if (Title === '' || Content === '') {
-      return alert('모든 항목을 채워주세요.');
+      return alert('모든 항목을 채워주세요.')
     }
     let body = {
       title: Title,
       content: Content,
-      image: Img,
+      image: Images,
       uid: user.uid,
-    };
+    }
+
     axios
       .post('/api/board/submit', body)
       .then((res) => {
         if (res.data.success) {
-          alert('글 작성이 완료되었습니다. 게시판으로 이동합니다.');
-          navigate('/board');
+          alert('글 작성이 완료되었습니다. 게시판으로 이동합니다.')
+          navigate('/board')
         } else {
-          alert('글 작성에 실패하였습니다.');
+          alert('글 작성에 실패하였습니다.')
         }
       })
       .catch((err) => {
-        console.log(err);
-      });
-  };
+        console.log(err)
+      })
+  }
 
   return (
     <>
       <Header />
       <div className="board-wrap">
         <h2 className="board_title">게시판 업로드</h2>
-        <form action="/api/board/submit" method="POST" encType="multipart/form-data">
+        <form>
           <label htmlFor="title">제목</label>
           <input
             id="title"
             type="text"
             value={Title}
             onChange={(e) => {
-              setTitle(e.currentTarget.value);
+              setTitle(e.currentTarget.value)
             }}
           />
-          {/* <ImgUpload setImg={setImg} /> */}
-          {/* <label htmlFor="image">Image</label> */}
-          {/* <input id="image" name="image" type="file" accept="image/*" onChange={(e) => FileUpload(e)} /> */}
-          <p>이미지 업로드 구현 예정</p>
+          <label htmlFor="image" className="img_label">
+            이미지 업로드
+          </label>
+          <input id="image" name="image" type="file" accept="image/*" onChange={(e) => FileUpload(e)} />
+          {File ? (
+            <img className="upload_preview" src={`${config.AWS_BOARD_URL}/${Images}`} alt="uploading..." />
+          ) : null}
           <label htmlFor="content">내용</label>
           <textarea
             id="content"
             wrap="hard"
             value={Content}
             onChange={(e) => {
-              setContent(e.currentTarget.value);
+              setContent(e.currentTarget.value)
             }}
           />
           <div className="btn_wrap">
             <button
               className="submit"
               onClick={(e) => {
-                onSubmit(e);
+                onSubmit(e)
               }}
             >
               제출
@@ -80,7 +105,7 @@ const Upload = () => {
         </form>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default Upload;
+export default Upload
